d20972d997b6f98797fc9e2bad3c3264549392d1
Gediminas Kazlauskas <gediminas.kazlauskas@biomapas.com>
2022-07-22 17:39:39 +0300

    PVMI-2296 Create AuthenaPythonSDK open-source package.

---
+++ authena_python_sdk_tests/unit/fixtures/__init__.py
@@ -0,0 +1,3 @@
+from authena_python_sdk_tests.unit.fixtures.sdk_client import *
+from authena_python_sdk_tests.unit.fixtures.user import *
+from authena_python_sdk_tests.unit.fixtures.group import *
